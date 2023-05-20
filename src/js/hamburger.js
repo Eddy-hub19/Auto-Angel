343b92cea1843558187f177ca390af3b1cefc5bb
EduardSekan <sekaneduard@gmail.com>
2023-05-20 14:39:53 +0300

    add hamburher logic and continued header,main,footer

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,14 +1,29 @@
 // Show Hamburger menu
-const hamburger = document.querySelector(".menu-btn")
-const menu = document.querySelector(".menu")
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("active")
-    menu.classList.toggle("active")
-})
+const hamburger = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
 
-// menu.addEventListener("click", function (event) {
-//     if (menu.classList.contains('active')) {
-//         menu.classList.remove('active');
-//         hamburger.classList.remove('active');
-//     }
-// })
+const closeButton = document.querySelector('.close-btn');
+closeButton.addEventListener('click', () => {
+  if (closeButton.classList.contains('white')) {
+    closeButton.classList.remove('white');
+    hamburger.style.background = 'transparent';
+  } else {
+    closeButton.classList.add('white');
+    hamburger.style.background = '#e8e8e8';
+  }
+});
+
+const light = document.querySelector('.light')
+light.document.style.background = "#262626"
+
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('active')) {
+    // hamburger.classList.remove('white');
+    menu.classList.remove('active');
+    hamburger.classList.remove('active');
+  }
+});
