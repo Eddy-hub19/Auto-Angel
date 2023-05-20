7ff18b17e6b7c20b461405fbecdf457efc7a404d
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2023-05-20 11:40:18 +0000

    Deploying to gh-pages from @ Eddy-hub19/Auto-Angel@343b92cea1843558187f177ca390af3b1cefc5bb 🚀

--- index.da4dc8d2.js
+++
@@ -1,2 +0,0 @@
-var hamburger=document.querySelector(".menu-btn"),menu=document.querySelector(".menu");hamburger.addEventListener("click",(function(){hamburger.classList.toggle("active"),menu.classList.toggle("active")}));
-//# sourceMappingURL=index.da4dc8d2.js.map
