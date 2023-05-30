534a33a38aff5e90e80861a74ea089900bb1e619
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2023-05-30 00:16:13 +0000

    Deploying to gh-pages from @ Eddy-hub19/Auto-Angel@c908b3ad84e842ac5e41e40ba9d0ffcc2f7e4711 🚀

--- index.cfb2e202.js
+++
@@ -1,2 +0,0 @@
-const section=document.querySelector(".content");section.onclick=e=>{const t=e.target;t.classList.contains("content-item__info")&&(console.log(t),navigator.clipboard.writeText(t.innerHTML).then((()=>{document.querySelector(".copied").style.display="inline-block";setTimeout((()=>{document.querySelector(".copied").style.display="none"}),800)})))};
-//# sourceMappingURL=index.cfb2e202.js.map
