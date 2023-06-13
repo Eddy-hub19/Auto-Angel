dfe2bb2311fbcdffe6f773b1c29af31285a8e99d
EduardSekan <sekaneduard@gmail.com>
2023-06-14 00:23:37 +0300

    add new func with hamburger for close-btn and scroll

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -5,12 +5,14 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   hamburger.classList.toggle('top');
   menu.classList.toggle('active');
+  document.body.classList.add('no-scroll');
 });
 
 const closeButton = document.querySelector('.close-btn');
 closeButton.addEventListener('click', () => {
   if (closeButton.classList.contains('white')) {
     closeButton.classList.remove('white');
+    document.body.classList.remove('no-scroll');
     hamburger.style.background = 'transparent';
   } else {
     closeButton.classList.add('white');
@@ -24,6 +26,7 @@ menuItem.forEach(item => {
   item.addEventListener('click', () => {
     hamburger.classList.remove('active');
     hamburger.classList.remove('white');
+    document.body.classList.remove('no-scroll');
     menu.classList.remove('active');
   });
 });
