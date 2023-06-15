c2c62688c9ab952db57e7e9394ddc4410837888f
Eddy-hub19 <75374150+Eddy-hub19@users.noreply.github.com>
2023-06-15 17:19:21 +0000

    Deploying to gh-pages from @ Eddy-hub19/Auto-Angel@52ff03382ebfb7c6bee7e9c1601d324c9fb0d786 🚀

---
+++ public-offer.2c7044ce.js
@@ -0,0 +1,2 @@
+let url=window.location.href,links=document.querySelectorAll(".nav__link");for(let l=0;l<links.length;l++){let e=links[l];e.href===url?e.classList.add("current"):e.classList.remove("current")}
+//# sourceMappingURL=public-offer.2c7044ce.js.map
--- public-offer.80d5d4da.js
+++
@@ -1,2 +0,0 @@
-let url=window.location.href,links=document.getElementsByTagName("a");for(let e=0;e<links.length;e++){let l=links[e];l.href===url?l.classList.add("current"):l.classList.remove("current")}
-//# sourceMappingURL=public-offer.80d5d4da.js.map
